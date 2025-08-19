dc33afbda1a91ac9d1eff780e154a155187d4abd
Jonathan Hatcher <jonathan.hatcher@jonatha-ltmrc9t.internal.salesforce.com>
2025-08-19 17:32:33 +1000

    Improve API error reporting for KV issues

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -27,7 +27,7 @@ export default async function handler(req, res){
     }
     return res.status(405).json({ error:'Method Not Allowed' });
   }catch(e){
-    return res.status(500).json({ error: 'Server error' });
+    return res.status(500).json({ error: 'Server error', message: e?.message });
   }
 }
 
